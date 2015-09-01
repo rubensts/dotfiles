1eb4467d40728fde1faeae684899e7c9f1af5345
Rubens Souza <rubensts@gmail.com>
2015-09-01 18:56:40 +0200

    Update spacemacs.d/init.el

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -41,7 +41,7 @@
      ipython-notebook
      ruby
      vagrant
-     my-org
+;     my-org
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
