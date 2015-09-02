e567f83a6533895b0c59caf3b34b032c1f2a0877
Rubens.TS <rubensts@gmail.com>
2015-09-02 08:09:41 +0200

    Altered filter on elfeed.org accordingly to project github page

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -109,7 +109,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks"))
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(custom-safe-themes
+   (quote
+    ("19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
