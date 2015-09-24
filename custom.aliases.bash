19694b6272beb26d30c52b0f6de5780ba07f5533
Rubens Souza <rubens.souza@2ndquadrant.it>
2015-09-24 19:03:52 +0200

    Config stumpwmrc, Xmodmap, conkerorrc and custom.aliases.bash

---
+++ custom.aliases.bash
@@ -0,0 +1,9 @@
+# This is my custom aliases file to be used with bash-it
+# Create a symlink as:
+# ln -s ~/git/dotfiles/custom.aliases.bash ~/.bash_it/aliases/custom.aliases.bash
+
+cite 'about-alias'
+about-alias 'my custom abbreviations'
+
+alias conkeror='firefox --app ~/git/conkeror/application.ini'
+alias ssh2q='ssh -F ~/prj/remote-dba/ssh_config'
