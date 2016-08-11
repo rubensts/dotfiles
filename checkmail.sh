802295b80ec513041d30e556b200358cd51d3e4d
Rubens.TS <rubensts@gmail.com>
2016-08-11 19:04:02 +0200

    Correct checkmail.sh script

--- checkmail.sh
+++ checkmail.sh
@@ -3,16 +3,9 @@
 # Keychain
 #eval `keychain --quiet --noask --eval --agents ssh,gpg id_rsa 6BB1CF3B`
 
-# Only run if connected
-#STATE=`nmcli -t -f state nm status`
-STATE=`nmcli -t -f state g status`
-
-if [ $STATE = 'connected' ]
-then
+# Check mail every minute
+while true; do
     mbsync -a
     notmuch new
-    exit 0
-fi
-echo "No internet connection."
-exit 0
-
+    sleep 60
+done
--- zshrc
+++ zshrc
@@ -81,7 +81,8 @@ antigen apply
 
 
 # Keychain
-#eval `keychain --eval --quiet --nogui --agents ssh,gpg id_rsa 6BB1CF3B`
+#eval `keychain --eval --quiet --noask --nogui --agents ssh,gpg id_rsa 6BB1CF3B`
+eval `keychain --eval --quiet --nogui --agents ssh,gpg id_rsa 6BB1CF3B`
 
 # Shows a fortune
 fortune
